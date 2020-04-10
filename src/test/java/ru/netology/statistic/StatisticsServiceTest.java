390607d1e6cd0d1b769688de4c1908365f0f7778
Yuliya Frolova <jupunkevich@gmail.com>
2020-04-10 23:01:03 +0300

    Updated test to 100% code coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,6 +15,13 @@ class StatisticsServiceTest {
 
         long actual = service.findMax(incomesInBillions);
 
+        long[] incomesInBillions2 = {11, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected2 = 12;
+
+        long actual2 = service.findMax(incomesInBillions2);
+
         assertEquals(expected, actual);
+        assertEquals(expected2, actual2);
     }
+
 }
